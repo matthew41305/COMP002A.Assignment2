fad7063c92fac6fb5b4af9a649887ec3c887de83
Matthew <matthew41305@gmail.com>
2024-10-28 16:27:53 -0700

    completed 1-5 with console.log

--- script.js
+++ script.js
@@ -3,19 +3,24 @@
 
 // 1. Write code to output the following string to the console:
 // "Hello World!"
+console.log("Hello World!");
 
 // 2. Write code to concatenate three strings and output the result to the console:
 // "Hello", " ", "World!"
+console.log("Hello" + "my" + "World!");
 
 // 3. Write code to output "Hello" on one line and "World!" on the next line to the console.
 // *Note: You should only have one console.log() statement - how do you add a new line in
 // a string?...
+console.log("Hello\nWorld");
 
 // 4. Write code to output the result of the arithmetic expression to the console:
 // 3 * 8 + 2;
+console.log(3 * 8 + 2); // Out: 26
 
 // 5. Write code to output the result of the comparison:
 // 5 is less than 10.
+console.log(3 * 8 + 2);
 
 // 6. Write code to output the result of the comparison:
 // 10 is greater than 5.
